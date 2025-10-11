ab3a344e6172e5c15aac80f5db24ef2eca5e9dee
LiaCost <lia.c.santtos@gmail.com>
2025-10-11 11:59:47 -0300

    Adição de comentários em classes restantes
    Foram adicionados comentários explicativos nas classes "Consulta" e "IAgendavel".

--- Teste_Software_Sarah/Teste_Software_Sarah/01_SistemaConsultorio/src/main/java/br/com/consultorio/agendamento/Consulta.java
+++ Teste_Software_Sarah/Teste_Software_Sarah/01_SistemaConsultorio/src/main/java/br/com/consultorio/agendamento/Consulta.java
@@ -2,33 +2,40 @@ package br.com.consultorio.agendamento;
 
 import br.com.consultorio.interfaces.IAgendavel;
 
+// Classe que representa uma consulta e implementa a interface IAgendavel
 public class Consulta implements IAgendavel {
 
-    private String data;
-    private String hora;
+    private String data; // Data da consulta
+    private String hora; // Hora da consulta
 
+    // Construtor 
     public Consulta(String data, String hora) {
         this.data = data;
         this.hora = hora;
     }
 
+    // Método da interface que agenda a consulta
     @Override
     public void agendarConsulta() {
-        System.out.println("Consulta agendada para " + data + " as " + hora + ".");
+        System.out.println("Consulta agendada para " + data + " às " + hora + ".");
     }
 
+    // Retorna a data
     public String getData() {
         return data;
     }
 
+    // Define a data
     public void setData(String data) {
         this.data = data;
     }
 
+    // Retorna a hora
     public String getHora() {
         return hora;
     }
 
+    // Define a hora
     public void setHora(String hora) {
         this.hora = hora;
     }

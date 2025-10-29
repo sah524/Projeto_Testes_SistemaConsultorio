c3c0bc248639158cf10cc275d937feb80362f1c0
Sarahh <183733899+sah524@users.noreply.github.com>
2025-10-29 09:40:54 -0300

    Remove pasta antiga Teste_Software_Sarah do repositório

--- Teste_Software_Sarah/Teste_Software_Sarah/01_SistemaConsultorio/src/main/java/br/com/consultorio/interfaces/IAgendavel.java
+++
@@ -1,8 +0,0 @@
-package br.com.consultorio.interfaces;
-
-// Interface que define o contrato para objetos que podem ser agendados
-public interface IAgendavel {
-
-    // Método que deve ser implementado para agendar uma consulta
-    void agendarConsulta();
-}
